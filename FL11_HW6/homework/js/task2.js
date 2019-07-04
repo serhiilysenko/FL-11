cf2f955a141357acb6ab464a2603ac88fd2c8505
serhiilysenko <diom34@gmail.com>
2019-07-04 23:53:22 +0300

    home work6

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,7 +1,7 @@
 let a =+prompt('Input lenght of side A', '');
 let b =+prompt('Input lenght of side B', '');
 let c =+prompt('Input lenght of side C', '');
-if (a+b<c||a+c<b||c+b<a) {
+if (a+b<=c||a+c<=b||c+b<=a) {
     console.log('Triangle doesn’t exist');
 } else if (a===b&&a===c&&c===b) {
     console.log('Eequivalent triangle');
