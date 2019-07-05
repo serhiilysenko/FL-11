b58db2d9065f96a304b4ca8059d48dfdcb48382a
serhiilysenko <diom34@gmail.com>
2019-07-05 23:34:55 +0300

    fixed some symbols

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,9 +1,9 @@
-let a1 = +prompt('input a X coordinate', '');
-let a2 = +prompt('input a Y coordinate', '');
-let b1 = +prompt('ipput b X coordinate', '');
-let b2 = +prompt('input b Y coordinate', '');
-let c1 = +prompt('input c X coordinate', '');
-let c2 = +prompt('input c Y coordinate', '');
+let a1 = +prompt('input a X coordinate');
+let a2 = +prompt('input a Y coordinate');
+let b1 = +prompt('ipput b X coordinate');
+let b2 = +prompt('input b Y coordinate');
+let c1 = +prompt('input c X coordinate');
+let c2 = +prompt('input c Y coordinate');
 let devider = 2;
 if ((a1+b1)/devider===c1 && (a2+b2)/devider===c2) {
     console.log('true'); 
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,11 +1,11 @@
-let a =+prompt('Input lenght of side A', '');
-let b =+prompt('Input lenght of side B', '');
-let c =+prompt('Input lenght of side C', '');
-if (a+b<=c||a+c<=b||c+b<=a) {
+let a =+prompt('Input lenght of side A');
+let b =+prompt('Input lenght of side B');
+let c =+prompt('Input lenght of side C');
+if (a+b <= c || a+c <= b || c+b <= a) {
     console.log('Triangle doesn’t exist');
-} else if (a===b&&a===c&&c===b) {
+} else if (a === b && a === c && c === b) {
     console.log('Eequivalent triangle');
-} else if (a===b&&a!==c||a===c&&a!==b||b===c&&b!==a) {
+} else if (a === b && a !==c || a === c && a !== b || b === c && b !== a) {
     console.log('Isosceles triangle');
 } else { 
     console.log('Normal traingle');
